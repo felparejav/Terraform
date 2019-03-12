4734461d80e10f2dcaedadc69934194e799a7d68
felparejav <andres.pareja@endava.com>
2019-03-12 14:41:24 -0500

    version final

--- config/config_minion.sh
+++ config/config_minion.sh
@@ -4,6 +4,6 @@ sudo yum clean expire-cache;
 sudo yum install salt-minion -y;
 sudo yum install salt-ssh -y;
 sudo yum install salt-api -y;
-sudo yum install git;
+sudo yum install git -y;
 sudo echo "master: 10.0.5.20" | sudo tee /etc/salt/minion;
 sudo systemctl restart salt-minion;
--- network/variables.tf
+++ network/variables.tf
@@ -4,30 +4,30 @@ variable "availability_zones" {
 
 variable "public_subnet_1a_cidr" {
   description = "CIDR for the Public Subnet"
-  default     = "10.0.1.0/24"
+  default     = "192.168.1.0/24"
 }
 
 variable "public_subnet_1b_cidr" {
   description = "CIDR for the Public Subnet"
-  default     = "10.0.2.0/24"
+  default     = "192.168.2.0/24"
 }
 
 variable "private_subnet_front_1a_cidr" {
   description = "CIDR for the Private Subnet"
-  default     = "10.0.3.0/24"
+  default     = "192.168.3.0/24"
 }
 
 variable "private_subnet_front_1b_cidr" {
   description = "CIDR for the Private Subnet"
-  default     = "10.0.4.0/24"
+  default     = "192.168.4.0/24"
 }
 
 variable "public_subnet_jenkins_1a_cidr" {
   description = "CIDR for the Private Subnet"
-  default     = "10.0.5.0/24"
+  default     = "192.168.5.0/24"
 }
 
 variable "private_subnet_back_1a_cidr" {
   description = "CIDR for the Private Subnet"
-  default     = "10.0.6.0/24"
+  default     = "192.168.6.0/24"
 }
