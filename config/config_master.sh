ce5a2656b46befae5642644d075945f20c214be6
felparejav <andres.pareja@endava.com>
2019-03-12 12:49:39 -0500

    aaaa

--- config/config_master.sh
+++ config/config_master.sh
@@ -6,7 +6,7 @@ sudo yum install salt-minion -y;
 sudo yum install salt-ssh -y;
 sudo yum install salt-api -y;
 sudo yum install git -y;
-sudo echo "master: 10.0.11.20" | sudo tee /etc/salt/minion;
+sudo echo "master: 10.0.5.20" | sudo tee /etc/salt/minion;
 sudo echo "interface: 0.0.0.0" | sudo tee /etc/salt/master;
 sudo systemctl restart salt-minion;
 sudo systemctl restart salt-master;
